b4b76029fcac249b1182c13f30f1d4168ce5f044
chuhan wang <WA0022AN@e.ntu.edu.sg>
2021-10-07 21:15:43 +0800

    update

--- src/main/java/Card/CardManager.java
+++ src/main/java/Card/CardManager.java
@@ -1,4 +1,5 @@
 package main.java.Card;
+
 import java.util.ArrayList;
 import java.util.List;
 
--- src/main/java/Game/GameRecord.java
+++ src/main/java/Game/GameRecord.java
@@ -1,5 +1,5 @@
 package Game;
-
+import
 public class GameRecord {
     private static ArrayList<Game> games;
 
