70a87a51c2c44af089fbaf481e42715bc7fd7d5f
chuhan wang <WA0022AN@e.ntu.edu.sg>
2021-10-07 21:29:20 +0800

    update

--- src/main/java/Game/Game.java
+++
@@ -1,5 +0,0 @@
-package Game;
-
-abstract class Game {
-    abstract void execute();
-}
---
+++ src/test/java/UI/GameMainMenuByeTest.java
@@ -0,0 +1,12 @@
+package UI;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameMainMenuByeTest {
+    @Test
+    void showByeMessage() {
+        assertTrue(true);
+    }
+}
