b1a2733a3941520ee90fa236785af1d743f4bd44
LAPTOP-V2NPSHUB\19656 <1965637476@qq.com>
2021-10-10 19:19:50 +0800

    basic structure

---
+++ src/main/java/ui/Menu.java
@@ -0,0 +1,12 @@
+package ui;
+
+/**
+ * Abstract class {@code Menu}. Defines the common behaviours of the system's menus.
+ *
+ */
+public abstract class Menu {
+    /**
+     * Shows the menu.
+     */
+    public abstract void enter();
+}
---
+++ src/main/java/utils/StringParser.java
@@ -0,0 +1,197 @@
+package sg.edu.ntu.scse.cz2002.rrpss.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * A string parser utility that implements some core functionalities of the
+ * built-in {@code StringTokenizer}, but also with some additional
+ * application-specific ones.
+ * 
+ * @author Zhong Ruoyu
+ * @version 2.1
+ * @see java.util.StringTokenizer
+ */
+public class StringParser {
+    /**
+     * The type of a token.
+     */
+    private enum TokenType {
+        /**
+         * Double quoted token.
+         */
+        DOUBLE_QUOTED,
+
+        /**
+         * Single quoted token.
+         */
+        SINGLE_QUOTED,
+
+        /**
+         * Unquoted token.
+         */
+        UNQUOTED;
+    }
+
+    /**
+     * A token consumed by the parser.
+     */
+    private class Token {
+        /**
+         * The type of this token.
+         */
+        TokenType tokenType;
+
+        /**
+         * The value of this token.
+         */
+        String token;
+
+        /**
+         * Initialises a token.
+         * 
+         * @param tokenType the type of the token
+         * @param token     the value of the token
+         */
+        Token(TokenType tokenType, String token) {
+            this.tokenType = tokenType;
+            this.token = token;
+        }
+    }
+
+    /**
+     * The RegEx pattern that matches any of the following, seperated by any number
+     * of whitespace characters:
+     * <ul>
+     * <li>"Double quoted tokens" with escape {@code \"};</li>
+     * <li>'Single quoted tokens' with escape {@code \'};</li>
+     * <li>Unquoted single tokens.</li>
+     * </ul>
+     * 
+     * The pattern is
+     * {@code ("[^"\\]*(?:\\.[^"\\]*)*")|('[^'\\]*(?:\\.[^'\\]*)*')|(\S+)}.
+     */
+    private static final String REGEX = String.format("(%s)|(%s)|(%s)", "\"[^\"\\\\]*(?:\\\\.[^\"\\\\]*)*\"",
+            "'[^'\\\\]*(?:\\\\.[^'\\\\]*)*'", "\\S+");
+
+    /**
+     * The string that needs to be processed by the parser.
+     */
+    private final String string;
+
+    /**
+     * The list of tokens parsed by the parser.
+     */
+    private final List<Token> tokens;
+
+    /**
+     * Pointer to the next unread token.
+     */
+    private int next = 0;
+
+    /**
+     * Initialises a parser with the string given.
+     * 
+     * @param string the string for the parser to consume
+     */
+    public StringParser(String string) {
+        this.string = string;
+        this.tokens = new ArrayList<>();
+
+        Matcher matcher = Pattern.compile(REGEX).matcher(string);
+        while (matcher.find()) {
+            String token = matcher.group();
+            TokenType tokenType;
+            if (matcher.group(1) != null) {
+                token = token.substring(1, token.length() - 1);
+                tokenType = TokenType.DOUBLE_QUOTED;
+            } else if (matcher.group(2) != null) {
+                token = token.substring(1, token.length() - 1);
+                tokenType = TokenType.SINGLE_QUOTED;
+            } else {
+                tokenType = TokenType.UNQUOTED;
+            }
+            if (!token.isBlank()) {
+                tokens.add(new Token(tokenType, token));
+            }
+        }
+    }
+
+    /**
+     * Finds if this parser has more unread tokens.
+     * 
+     * @return {@code true} if there are more tokens to be read; {@code false}
+     *         otherwise
+     * @see java.util.StringTokenizer#hasMoreTokens()
+     */
+    public boolean hasMoreTokens() {
+        return next < tokens.size();
+    }
+
+    /**
+     * Gets the next unread token from this parser.
+     * 
+     * @return the next token string, if exists; {@code null} otherwise
+     * @see java.util.StringTokenizer#nextToken()
+     */
+    public String nextToken() {
+        if (hasMoreTokens()) {
+            return tokens.get(next++).token;
+        }
+        return null;
+    }
+
+    /**
+     * Calculates the number of times that this parser's {@code nextToken} method
+     * can be called before it returns {@code null}. The current position is not
+     * advanced.
+     *
+     * @return the number of tokens remaining in the string using the current
+     *         delimiter set.
+     * @see java.util.StringTokenizer#countTokens()
+     */
+    public int countTokens() {
+        return tokens.size();
+    }
+
+    /**
+     * Gets the underlying string of this parser.
+     * 
+     * @return the underlying string
+     */
+    public String getString() {
+        return string;
+    }
+
+    /**
+     * Gets the unread tokens, combined as a string.
+     * 
+     * @return the unread tokens, combined as a string
+     */
+    public String getRemaining() {
+        List<Token> remainingTokens = tokens.subList(next, tokens.size());
+        List<String> remainingStrings = new ArrayList<>();
+        for (Token token : remainingTokens) {
+            String quotes;
+            switch (token.tokenType) {
+                case DOUBLE_QUOTED: {
+                    quotes = "\"";
+                    break;
+                }
+                case SINGLE_QUOTED: {
+                    quotes = "'";
+                    break;
+                }
+                case UNQUOTED:
+                default: {
+                    quotes = "";
+                    break;
+                }
+            }
+            remainingStrings.add(String.format("%s%s%s", quotes, token.token, quotes));
+        }
+        return String.join(" ", remainingStrings);
+    }
+}
