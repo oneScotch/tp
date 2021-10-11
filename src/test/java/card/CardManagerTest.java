220530a1ba03dde88e67ceeedd4e3591aa52c560
Lily Peng <c200218@e.ntu.edu.sg>
2021-10-12 02:06:30 +0800

    minor change

--- src/test/java/card/CardManagerTest.java
+++ src/test/java/card/CardManagerTest.java
@@ -2,8 +2,7 @@ package card;
 
 import data.card.Card;
 import data.card.CardManager;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 
@@ -14,16 +13,17 @@ class CardManagerTest {
 
     @BeforeEach
     void setUp() {
-         ArrayList<Card> cardList = new ArrayList<>();
-         cards.add(new Card("Loops can execute a block of code as long as a specified condition is reached."));
-         cards.add(new Card("Arrays are used to store multiple values in a single variable"));
-         cards.add(new Card("Use the if statement to specify a block of Java code to be executed if a condition is true."));
-         cards = new CardManager(cardList);
+        ArrayList<Card> cardList = new ArrayList<>();
+        cardList.add(new Card("Loops can execute a block of code as long as a specified condition is reached."));
+        cardList.add(new Card("Arrays are used to store multiple values in a single variable"));
+        cardList.add(new Card("Use the if statement to specify code to be executed if a condition is true."));
+        cards = new CardManager(cardList);
     }
 
     @Test
     public void testDeleteCard() {
-        Assertions.assertEquals(cards.deleteCard(1), "Loops can execute a block of code as long as a specified condition is reached.");
+        Assertions.assertEquals(cards.deleteCard(1),
+                "Loops can execute a block of code as long as a specified condition is reached.");
     }
 
     @Test
