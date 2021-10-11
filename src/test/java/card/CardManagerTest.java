5b7e76cf6e757df9dcf4b90bf9a5b8790fd8707d
Lily Peng <c200218@e.ntu.edu.sg>
2021-10-12 01:36:26 +0800

    update card test

--- src/test/java/card/CardManagerTest.java
+++ src/test/java/card/CardManagerTest.java
@@ -1,7 +1,33 @@
 package card;
 
+import data.card.Card;
+import data.card.CardManager;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.ArrayList;
 
 class CardManagerTest {
+    CardManager cards;
 
+    @BeforeEach
+    void setUp() {
+         ArrayList<Card> cardList = new ArrayList<>();
+         cards.add(new Card("Loops can execute a block of code as long as a specified condition is reached."));
+         cards.add(new Card("Arrays are used to store multiple values in a single variable"));
+         cards.add(new Card("Use the if statement to specify a block of Java code to be executed if a condition is true."));
+         cards = new CardManager(cardList);
+    }
+
+    @Test
+    public void testDeleteCard() {
+        Assertions.assertEquals(cards.deleteCard(1), "Loops can execute a block of code as long as a specified condition is reached.");
+    }
+
+    @Test
+    public void testExchange() {
+        Assertions.assertTrue(cards.exchange(1));
+    }
 }
--- src/test/java/card/CardTest.java
+++ src/test/java/card/CardTest.java
@@ -2,10 +2,17 @@ package card;
 
 import data.card.Card;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 
 class CardTest {
+    @Test
     public void testOutput() {
         Assertions.assertEquals(new Card("loop"), "loop");
+    }
+
+    @Test
+    public void testIsUsed() {
+        Assertions.assertFalse(new Card("loop").checkIfIsUsed());
     }
 }
