b45de0e1e48756119f134d6e9c9d2ff0f0480dc1
Peng Wenxuan <70517391+LilyDaytoy@users.noreply.github.com>
2021-10-12 02:02:34 +0800

    Update CardManagerTest.java
    minor change

--- src/test/java/card/CardManagerTest.java
+++ src/test/java/card/CardManagerTest.java
@@ -13,16 +13,17 @@ class CardManagerTest {
 
     @BeforeEach
     void setUp() {
-         ArrayList<Card> cardList = new ArrayList<>();
-         cards.add(new Card("Loops can execute a block of code as long as a specified condition is reached."));
-         cards.add(new Card("Arrays are used to store multiple values in a single variable"));
-         cards.add(new Card("Use the if statement to specify a block of Java code to be executed if a condition is true."));
-         cards = new CardManager(cardList);
+        ArrayList<Card> cardList = new ArrayList<>();
+        cards.add(new Card("Loops can execute a block of code as long as a specified condition is reached."));
+        cards.add(new Card("Arrays are used to store multiple values in a single variable"));
+        cards.add(new Card("Use the if statement to specify a block of Java code to be executed if a condition is true."));
+        cards = new CardManager(cardList);
     }
 
     @Test
     public void testDeleteCard() {
-        Assertions.assertEquals(cards.deleteCard(1), "Loops can execute a block of code as long as a specified condition is reached.");
+        Assertions.assertEquals(cards.deleteCard(1), 
+                                "Loops can execute a block of code as long as a specified condition is reached.");
     }
 
     @Test
