55dd68d9130291fe186baaf91651fad9f916f5c8
Lily Peng <c200218@e.ntu.edu.sg>
2021-10-11 23:32:43 +0800

    player update

--- src/main/java/data/card/Card.java
+++ src/main/java/data/card/Card.java
@@ -5,8 +5,8 @@ import java.io.Serializable;
 
 public class Card implements Serializable {
     private static final long serialVersionUID = -9135686500512288865L;
-    private String codeTip;
-    private int cardType;
+    private String content;
+    private boolean isUsed;
 
     public Card(String content) {
         this.content = content;
