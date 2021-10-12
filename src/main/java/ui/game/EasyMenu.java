33e9f33bc886b5635fecae11796e398cfb0b7d5d
Yuxinn-J <C200203@e.ntu.edu.sg>
2021-10-12 20:00:33 +0800

    add EasyMenu ui v0.5

--- src/main/java/ui/game/EasyMenu.java
+++ src/main/java/ui/game/EasyMenu.java
@@ -1,17 +1,52 @@
 package ui.game;
 
+import data.game.HangmanGame;
 import ui.Menu;
 import utils.StringParser;
+import utils.message.Strings;
 
 import java.util.Scanner;
 
+/**
+ * The easy game mode menu of the CodeHunt game.
+ */
 public class EasyMenu extends Menu {
-    public EasyMenu(Scanner in, StringParser parser) {
+    /**
+     * The {@code Scanner} for the menu to consume input.
+     */
+    private final Scanner in;
 
+    /**
+     * The parser with the rest of the command to be consumed by the menu.
+     */
+    private final StringParser parser;
+
+    /**
+     * Creates a new instance of the easy menu.
+     */
+    public EasyMenu(Scanner in, StringParser parser) {
+        this.in = in;
+        this.parser = parser;
     }
 
     @Override
     public void enter() {
+        welcome();
 
+        System.out.println("Level 1: Hang Man");
+        HangmanGame hangmanGame = new HangmanGame();
+        hangmanGame.execute();
+
+        System.out.println("Well done! You have finished the first level!");
+    }
+
+    /**
+     * Displays the welcome message and the background,
+     */
+    private void welcome() {
+        System.out.println(Strings.EASY_LOGO);
+        System.out.println(Strings.EASY_GAME_WELCOME_MESSAGE);
+        System.out.println();
     }
 }
+
