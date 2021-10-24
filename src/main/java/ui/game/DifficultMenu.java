4b26e070276337083867ee777a40952c6f783936
Yuxinn-J <C200203@e.ntu.edu.sg>
2021-10-24 22:10:23 +0800

    add DifficultUi structure

--- src/main/java/ui/game/DifficultMenu.java
+++ src/main/java/ui/game/DifficultMenu.java
@@ -2,17 +2,43 @@ package ui.game;
 
 import ui.Menu;
 import utils.StringParser;
+import utils.message.Strings;
 
 import java.util.Scanner;
 
+/**
+ * The difficult game mode menu of the CodeHunt game.
+ */
 public class DifficultMenu extends Menu {
+    /**
+     * The {@code Scanner} for the menu to consume input.
+     */
+    private final Scanner in;
 
+    /**
+     * The parser with the rest of the command to be consumed by the menu.
+     */
+    private final StringParser parser;
+
+    /**
+     * Creates a new instance of the difficult menu.
+     */
     public DifficultMenu(Scanner in, StringParser parser) {
-
+        this.in = in;
+        this.parser = parser;
     }
 
     @Override
     public void enter() {
 
+    }
+
+    /**
+     * Displays the welcome message and the background.
+     */
+    private void welcome() {
+        System.out.println(Strings.EASY_LOGO);
+        System.out.println(Strings.EASY_GAME_WELCOME_MESSAGE);
+        System.out.println();
     }
 }
