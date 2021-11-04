40db5762f959516ca14c3e213a1b91ef2a3c4197
Mist0919 <XUEY0013@e.ntu.edu.sg>
2021-11-05 00:29:40 +0800

    Fix bugs in Guessing Number Game and Treasure Hunt Game & add difficult level

--- src/main/java/data/game/TreasureHuntGame.java
+++ src/main/java/data/game/TreasureHuntGame.java
@@ -8,12 +8,21 @@ public class TreasureHuntGame extends Game implements Serializable {
     public static final String name = "Treasure Hunt";
     public static final String GAME_RULES = "The purpose of the game is to control the treasure hunter's"
             + " to get the treasure. The position of the treasure is marked as '*' on the map, "
-            + "you need to choose a proper sequence of movement to reach that position.";
+            + "you need to choose a proper sequence of movement to reach that position."
+            + "Notice: the path is one-time, that is, you will fall if you try going across the path covered.";
     private static int[][] map;
     private static int startID = 1;   // the start ID
 
-    public TreasureHuntGame() {
-        this.map = new TreasureHuntMap().getMap();
+    /**
+     * Constructs a treasure hunt game according to whether it is difficult.
+     * @param boolean isDifficult identifies whether the game is at difficult level
+     */
+    public TreasureHuntGame(boolean isDifficult) {
+        if (!isDifficult) {
+            this.map = new TreasureHuntMap().getEasyMap();
+        } else {
+            this.map = new TreasureHuntMap().getDifficultMap();
+        }
     }
 
     /**
@@ -23,11 +32,10 @@ public class TreasureHuntGame extends Game implements Serializable {
     @Override
     public int execute() {
         displayGameDetails();
-        displayMovements();
         displayMapDetails();
+        displayMovements();
         displayMap();
-        TreasureHuntGame g = new TreasureHuntGame();
-        boolean isWin = g.play();    //changed
+        boolean isWin = play();    //changed
         return isWin ? startID : 0;    // return startID if win return 0 indicates lose
     }
 
@@ -54,6 +62,7 @@ public class TreasureHuntGame extends Game implements Serializable {
         while (!find) {
             String input = in.nextLine();
             if (!isValidCommand(input)) {
+                inValidCommandMessage();
                 continue;
             }
             for (int i = 0; i < input.length(); i++) {
@@ -103,34 +112,11 @@ public class TreasureHuntGame extends Game implements Serializable {
     }
 
     /**
-     * parses the command entered by the user.
-     * @return int array of the encoded command sequence
-     */
-    public int[] parseCommand(String input) {
-        String commandString = input.replace(" ", "");
-        int[] commandArray = new int[commandString.length()];
-        for (int index = 0; index < commandString.length(); index++) {
-            if (commandString.charAt(index) == '1') {
-                commandArray[index] = 1;
-            } else if (commandString.charAt(index) == '2') {
-                commandArray[index] = 2;
-            } else if (commandString.charAt(index) == '3') {
-                commandArray[index] = 3;
-            } else if (commandString.charAt(index) == '4') {
-                commandArray[index] = 4;
-            } else {
-                return commandArray;
-            }
-        }
-        return commandArray;
-    }
-
-    /**
      * determines whether the move is available.
      * @return boolean whether the move is available
      */
     public boolean canMove(int x, int y) {
-        if (this.map[y][x] != 1) {
+        if (this.map[y][x] == 0 || this.map[y][x] == -1) {
             return true;
         } else {
             return false;
@@ -149,7 +135,7 @@ public class TreasureHuntGame extends Game implements Serializable {
      * @return boolean whether the treasure is found
      */
     public boolean found(int x, int y) {
-        if (this.map[x][y] == -1) {
+        if (this.map[y][x] == -1) {
             return true;
         } else {
             return false;
@@ -174,9 +160,17 @@ public class TreasureHuntGame extends Game implements Serializable {
      * prints the message that the input move is not available.
      */
     public void printCannotMoveMessage() {
-        System.out.println("Sorry, you cannot move like that :(");
+        System.out.println("Oops, you cannot move like that!"
+                + "Game over:(");
     }
 
+    /**
+     * prints the message that the input command is invalid.
+     */
+    public void inValidCommandMessage() {
+        System.out.println("Sorry, your input is invalid. Please enter a sequence of movements.\n"
+                + "e.g. 13342 (move right-up-up-down-left)");
+    }
     /**
      * prints the message that the treasure hunter have found the treasure.
      */
@@ -229,9 +223,9 @@ public class TreasureHuntGame extends Game implements Serializable {
      */
     public void displayMapDetails() {
         System.out.println("Introduction of the map:\n"
-            + "x: walls. You cannot go across it.\n"
+            + "x: traps. Be careful and stay away from them!\n"
             + "O: start position of the treasure hunter.\n"
-            + "*: treasure position. You need get to there!\n"
-            + "·: noting the path you have covered.\n");
+            + "*: treasure position. You need get there!\n"
+            + "·: noting the path you have covered. You cannot go back onto the previous paths.\n");
     }
 }
